9957f6157b525215c84804f14b480c36edd0da8f
Dirk Wilden <dirkwilden@gmail.com>
2020-03-26 12:49:18 +0100

    add all log events

--- client.js
+++ client.js
@@ -1,25 +1,39 @@
-
 function startClient() {
 
-  let socket = require('socket.io-client')('http://127.0.0.1:9009');
-  socket.on('connect', function(){
-    console.log("connected");
-  });
-  socket.on('say', function(data){
-    console.log("say: " + JSON.stringify(data));
-  });
-  socket.on('playerBegin', function(data){
-    console.log("playerBegin: " + JSON.stringify(data));
-  });
-  socket.on('playerDisconnected', function(data){
-    console.log("playerDisconnect: " + JSON.stringify(data));
-  });
-  socket.on('gameStart', function(data){
-    console.log("gameStart: " + JSON.stringify(data));
-  });
-  socket.on('disconnect', function(){
-    console.log("disconnected");
-  });
+    let socket = require('socket.io-client')('http://127.0.0.1:9009');
+    socket.on('connect', function () {
+        console.log("connected");
+    });
+    socket.on('say', function (data) {
+        console.log("say: " + JSON.stringify(data));
+    });
+    socket.on('playerBegin', function (data) {
+        console.log("playerBegin: " + JSON.stringify(data));
+    });
+    socket.on('playerInfoChanged', function (data) {
+        console.log("playerInfoChanged: " + JSON.stringify(data));
+    });
+    socket.on('playerConnect', function (data) {
+        console.log("playerConnect: " + JSON.stringify(data));
+    });
+    socket.on('playerDisconnect', function (data) {
+        console.log("playerDisconnect: " + JSON.stringify(data));
+    });
+    socket.on('gameStart', function (data) {
+        console.log("gameStart: " + JSON.stringify(data));
+    });
+    socket.on('gameEnd', function (data) {
+        console.log("gameEnd: " + JSON.stringify(data));
+    });
+    socket.on('item', function (data) {
+        console.log("item: " + JSON.stringify(data));
+    });
+    socket.on('kill', function (data) {
+        console.log("kill: " + JSON.stringify(data));
+    });
+    socket.on('disconnect', function () {
+        console.log("disconnected");
+    });
 }
 
 startClient();
